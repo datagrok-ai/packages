68105eb6f33a6545629b94a9e922a9b8b4c27fa4
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-01-07 18:40:22 +0200

    Add sh script that does the substitution

---
+++ .github/scripts/publish-from-branch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+current_branch=$(git branch --show-current 2>/dev/null)
+if [ -z "$current_branch" ]; then
+  echo "Error: Failed to determine the current Git branch. Make sure you are in a Git repository."
+  exit 1
+fi
+
+echo "Current branch: $current_branch"
+
+line_to_find="if \[\[ '\${{ github.ref }}' == 'refs/heads/master' \]\]; then"
+line_to_replace="if \[\[ '\${{ github.ref }}' == 'refs/heads/$current_branch' \]\]; then"
+
+file_path="../workflows/packages.yaml"
+
+if grep -q "$line_to_find" "$file_path"; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" "s|$line_to_find|$line_to_replace|g" "$file_path"
+  else
+    sed -i "s|$line_to_find|$line_to_replace|g" "$file_path"
+  fi
+
+  echo "Successfully updated the reference in $file_path."
+else
+  echo "The line with 'refs/heads/master' was not found in $file_path."
+fi
