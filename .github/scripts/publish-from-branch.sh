67c2d4f1097f3015a23c9cc45f960b16f3c0682e
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-01-07 19:16:05 +0200

    Update text in the shell script

--- .github/scripts/publish-from-branch.sh
+++ .github/scripts/publish-from-branch.sh
@@ -2,7 +2,7 @@
 
 current_branch=$(git branch --show-current 2>/dev/null)
 if [ -z "$current_branch" ]; then
-  echo "Error: Failed to determine the current Git branch. Make sure you are in a Git repository."
+  echo "Error: Failed to determine the current git branch."
   exit 1
 fi
 
@@ -20,7 +20,7 @@ if grep -q "$line_to_find" "$file_path"; then
     sed -i "s|$line_to_find|$line_to_replace|g" "$file_path"
   fi
 
-  echo "Successfully updated the reference in $file_path."
+  echo "Successfully updated ${file_path} to use the branch '${current_branch}'."
 else
   echo "The line with 'refs/heads/master' was not found in $file_path."
 fi
