1f45b756872a6e9c701a846bf0c13fbf90f3f271
Vladyslav Hlushchenko <vhlushchenko@datagrok.ai>
2025-01-31 18:15:21 +0200

    NOSTORY: AnalyzeMethods

--- bash.sh
+++
@@ -1,5 +0,0 @@
-# Присваиваем переменной последнюю версию папки, содержащей "mac"
-last_version=$(ls ~/.cache/puppeteer/chrome | grep mac | sort -V | tail -n 1)
-
-# Выводим значение переменной для проверки
-echo "Последняя версия: $last_version"
